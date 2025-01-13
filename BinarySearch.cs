0af379abf7ebf5d6d66e93a47516e9ed94a32ebe
JerinJomy <jerinjomy123@outlook.com>
2025-01-13 21:07:25 +0530

    Rotated sorted array

--- BinarySearch.cs
+++ BinarySearch.cs
@@ -66,6 +66,7 @@ public static class BinarySearch
 
         return ans;
     }
+    
      public static int LastOccurance(int[] arr, int target)
     {
         int low = 0, high = arr.Length - 1, ans = -1;
@@ -88,5 +89,43 @@ public static class BinarySearch
 
         return ans;
     }
+
+    public static int FindInRotatedArray(int[] arr, int target)
+    {
+        int low = 0, high = arr.Length - 1, ans = -1;
+        while (low <= high)
+        {
+            var mid = (low + high) / 2;
+            if (arr[mid] == target)
+            {
+                return mid;
+            }
+            if (arr[mid] <= arr[high])
+            {
+                if ( target >= arr[mid] &&  target <= arr[high])
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+            else
+            {
+                if (target >= arr[low] && target <= arr[mid])
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    high = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
+     
+     
     
 }
